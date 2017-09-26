dd01da2362a1f37325d72dc86b8d1b7bdb14c092
Stefan Martynkiw <linster247@gmail.com>
2017-09-25 19:05:09 -0600

    Done section 5

--- src/v_builders/n37StringAndMapBuilders.kt
+++ src/v_builders/n37StringAndMapBuilders.kt
@@ -2,6 +2,7 @@ package v_builders
 
 import util.TODO
 import java.util.*
+import kotlin.collections.HashMap
 
 fun buildStringExample(): String {
     fun buildString(build: StringBuilder.() -> Unit): String {
@@ -9,6 +10,8 @@ fun buildStringExample(): String {
         stringBuilder.build()
         return stringBuilder.toString()
     }
+
+
 
     return buildString {
         this.append("Numbers: ")
@@ -28,12 +31,18 @@ fun todoTask37(): Nothing = TODO(
     """
 )
 
-fun task37(): Map<Int, String> {
-    todoTask37()
-//    return buildMap {
-//        put(0, "0")
-//        for (i in 1..10) {
-//            put(i, "$i")
-//        }
-//    }
+fun buildMap(build: HashMap<Int, String>.() -> Unit): HashMap<Int, String>{
+    val map = HashMap<Int, String>()
+    map.build()
+    return map
 }
+
+fun task37(): Map<Int, String> {
+    return buildMap {
+        put(0, "0")
+        for (i in 1..10) {
+            put(i, "$i")
+        }
+    }
+}
+
